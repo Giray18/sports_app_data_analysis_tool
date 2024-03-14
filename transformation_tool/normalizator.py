dc63458190fa7e7ac79168eada93aeccbfe287b6
Giray18 <cihangiray.oner@gmail.com>
2024-03-14 15:55:27 +0100

    transformation_tool_removed

--- transformation_tool/normalizator.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-import glob
-import numpy as np
-import dat
-
-def normalize_needed_cols(df: pd.DataFrame):
-    ''' This function reads selected columns from dataframes and normalize '''
